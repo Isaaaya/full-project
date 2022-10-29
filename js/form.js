bfa67a373f6b8f1e806d1d3bf2c8eddc0d03749c
Isaaaya <snizhanna.bushko@gmail.com>
2022-10-29 22:48:52 +0300

    Worked on the form validation with JS

--- js/form.js
+++ js/form.js
@@ -1,78 +1,89 @@
 // const e = require("express");
 
-(function () {
+// (function () {
+  const nameInput = document.querySelector(".client-name");
+  const phoneInput = document.querySelector(".client-phone");
+  const emailInput = document.querySelector(".client-email");
+  const nameError = document.querySelector(".client-name-error");
+  const phoneError = document.querySelector(".client-phone-error");
+  const emailError = document.querySelector(".client-email-error");
+  const dateError = document.querySelector(".client-date-error");
+  const submitError = document.querySelector(".submit-error");
 
-    const form = document.querySelector(".contact-us__form");
-    const name = document.querySelector(".client-name");
-    const phone = document.querySelector(".client-phone");
-    const email = document.querySelector(".client-email");
-
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-
-        validateInputs();
-    });
-
-    const setError = (element, message) => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector(".error")
-
-        errorDisplay.innerText = message;
-        inputControl.classList.add("error");
-        inputControl.classList.remove("success");
+  function validateName() {
+    const name = document.querySelector(".client-name").value;
+    if (name.length === 0) {
+        nameError.innerHTML = "Name is required";
+        return false;
     }
-
-    const setSuccess = element = () => {
-        const inputControl = element.parentElement;
-        const errorDisplay = inputControl.querySelector(".error");
-
-        errorDisplay.innerText = "";
-        inputControl.classList.add("success");
-        inputControl.classList.remove("error");
+    if (name.length < 5) {
+        nameError.innerHTML = "Name must consist of at least 5 letters";
+        return false;
     }
-
-    const isValidEmail = email => {
-        const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-        return re.test(String(email).toLowerCase());
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = "Enter a full name";
+        return false;
     }
-
-    const validateInputs = () => {
-        const nameValue = name.value.trim();
-        const phoneValue = phone.value.trim();
-        const emailValue = email.value.trim();
-
-        if (nameValue === "") {
-            setError(name, "Name is required");
-        }
-        else if (nameValue.length < 5) {
-            setError(name, "Name must contain of at least 5 characters");
-        }
-        else {
-            setSuccess(name);
-        };
-    
-        if (phoneValue === "") {
-            setError(phone, "Phone is required");
-        }
-        else if (phoneValue.length < 10) {
-            setError(phone, "Phone number must consist of at least 10 characters");
-        }
-        else {
-            setSuccess(phone);
-        };
-
-        if (emailValue === "") {
-            setError(email, "Email is required");
-        }
-        else if (!isValidEmail(emailValue)) {
-            setError(email, "Provide a valid email address");
-        }
-        else {
-            setSuccess(email);
-        };
-    
+   else {
+    nameError.classList.add("success");
+    nameError.innerHTML = "<span class='success'>✓</span>";
+    return true;
+   }
+  }
+  
+  function validatePhone() {
+    const phone = document.querySelector(".client-phone").value;
+    if (phone.length === 0) {
+        phoneError.innerHTML = "Phone number is required";
+        return false;
     }
-}
+    if (phone.length !== 10) {
+        phoneError.innerHTML = "Phone must consist of 10 digits";
+        return false;
+    }
+    if (!phone.match(/^[0-9]{10}$/)) {
+        phoneError.innerHTML = "Only digits";
+        return false;
+    }
+   else {
+    phoneError.classList.add("success");
+    phoneError.innerHTML = "<span class='success'>✓</span>";
+    return true;
+   }
+  }
+
+  function validateEmail() {
+    const email = document.querySelector(".client-email").value;
+    if (email.length === 0) {
+        emailError.innerHTML = "Email is required";
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = "Email is invalid";
+        return false;
+    }
+   else {
+    emailError.classList.add("success");
+    emailError.innerHTML = "<span class='success'>✓</span>";
+    return true;
+   }
+  }
 
 
-)()
+  function validateForm() {
+    if (!validateName()) {
+        nameInput.classList.add("red-border");
+        return false;
+    }
+    else if (!validatePhone()) {
+        phoneInput.classList.add("red-border");
+        return false;
+    }
+    else if (!validateEmail()) {
+        emailInput.classList.add("red-border");
+        return false;
+    }
+  }
+
+// }
+// )()
